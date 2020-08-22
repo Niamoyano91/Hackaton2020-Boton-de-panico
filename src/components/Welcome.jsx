6a1b5aaceae59872c1ee5870b267c411c8517117
valeriaconstanzabc <valeria.brito.c@gmail.com>
2020-08-22 10:09:45 -0400

    cambios css portada + botón de pánico

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
 import '../styles/Welcome.css'
 import { Link } from "react-router-dom";
-import Grupo from '../assets/Group.png'
+import Grupo from '../assets/group.png'
 
 const Welcome = () => {
 
     return (
         <div className="containerWelcome">
             <h1 className='titleName'>Sis</h1>
+            <p className="subtittleWelcome">Una hermana te acompaña</p>
             <div className="containerImgLogoWelcome">
                 <img src={Grupo} className="logoWelcome" alt="Logo" />
             </div>
-            <h1 className='tituloPrincipal'>¡Edúcate y empodérate!</h1>
             <div className='containertext'>
-                <p className='parrafoPrincipal'>Una iniciativa de</p>
-                <h2 className='tituloUno'>OVO CHILE</h2>
+                <p className='parrafoPrincipal'>Una iniciativa</p>
                 <p className='parrafoPrincipal'>para prevenir la violencia obstétrica</p>
             </div>
             <Link to="/login">
