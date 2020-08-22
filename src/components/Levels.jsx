ee4863aa1f702bbed56ca5cf52b85a75b7fe31fe
estefania moyano <estefaniamoyano@MacBook-Air-de-estefania.local>
2020-08-22 09:27:59 -0400

    preguntas

--- src/components/Levels.jsx
+++ src/components/Levels.jsx
@@ -1,13 +1,33 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import '../styles/Levels.css'
 import firebase from 'firebase'
 import { UserContext } from '../Context/UserContext.js'
-import { Link } from 'react-router-dom'
 
 const Levels = () => {
     let { setCheck } = useContext(UserContext)
 
-    var user = firebase.auth().currentUser;
+    const [questions, setQuestions] = React.useState([])
+
+
+    console.log(questions)
+
+    useEffect(() => {
+        //<----------------Questions----------------------------->
+        const getQuestions = async () => {
+            try {
+                const db = firebase.firestore()
+                const data = await db.collection('preguntas').get()
+                const arrayData = data.docs.map(doc => ({ id: doc.id, ...doc.data() }))
+                setQuestions(arrayData)
+                console.log(arrayData)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getQuestions()
+    }, [])
+
+    const user = firebase.auth().currentUser;
     user.providerData.forEach(function (profile) {
         const name = profile.displayName;
         const email = profile.email;
@@ -21,29 +41,40 @@ const Levels = () => {
     return (
         <div className="containerLevels">
             <section>Progress Bar</section>
-            <main className="">
-                <h1>Nivel 1</h1>
-                <h2>¿Qué es la violencia obstétrica?</h2>
-                <div>
-                    <section className="multimedia">VIDEO
-                        <p className="infoMultimedia">¿Quieres saber más?</p>
-                    </section>
-                    <section className="questions">
-                        <h3>Preguntas</h3>
-                        <h4>1.La violencia obstetrica es:</h4>
-                        <input type="radio" id="" name="" value="" />
-                        <label for="">blablabla</label><br></br><br></br>
-                        <input type="checkbox" className="add-item__input"
-                            onChange = {e => setCheck(e.target.value)} value="check" 
-                        />
-                        <Link to="/home">
-                        <button>Enviar Respuestas</button>
-                        </Link>
-                    </section>
-                </div>
-            </main>
+            <h1>Nivel 1</h1>
+            <section className="multimedia">VIDEO
+<p className="infoMultimedia">¿Quieres saber más?</p>
+            </section>
+            {
+                questions.map((item, index) => (
+                    <main className="">
+                        <h2>{item.pregunta}</h2>
+                        <div>
+
+                            <section className="questions">
+                                <h3>Preguntas</h3>
+                                <h4>{item.A}</h4>
+                                <input type="radio" id="" name="" value="" />
+                                <label key={index} for="">{item.res1}</label><br></br>
+                                <input type="radio" id="" name="" value="" />
+                                <label key={index} for="">{item.res2}</label><br></br>
+                                <input type="radio" id="" name="" value="" />
+                                <label key={index} for="">{item.res3}</label><br></br>
+                                <input type="radio" id="" name="" value="" />
+                                <label key={index} for="">{item.res4}</label><br></br>
+                            </section>
+                        </div>
+                    </main>))
+            }
+            <button>Enviar Respuestas</button>
+            <input type="checkbox" className="add-item__input"
+                onChange={e => setCheck(e.target.value)} value="check"
+            />
         </div>
+
+
     )
 }
+
 
 export default Levels
